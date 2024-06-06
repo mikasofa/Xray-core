5de9a27b3fe6a114bf7f4c08bd441bb6dbf22c21
Loongqing <mankeunokmand@hotmail.com>
2024-06-07 01:21:37 +0800

    change name back

--- features/routing/session/context.go
+++ features/routing/session/context.go
@@ -3,9 +3,9 @@ package session
 import (
 	"context"
 
-	"github.com/mikasofa/xray-core/common/net"
-	"github.com/mikasofa/xray-core/common/session"
-	"github.com/mikasofa/xray-core/features/routing"
+	"github.com/xtls/xray-core/common/net"
+	"github.com/xtls/xray-core/common/session"
+	"github.com/xtls/xray-core/features/routing"
 )
 
 // Context is an implementation of routing.Context, which is a wrapper of context.context with session info.
